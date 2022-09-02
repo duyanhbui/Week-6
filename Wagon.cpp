5128551663cfed9c3ee9e1baf2cc8f21dd224bc5
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-09-02 20:07:54 +0930

    ahhhh

--- Wagon.cpp
+++ Wagon.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include "Wagon.h"
 Wagon :: Wagon(){
-    Capybara_records = new Capybara[wagon_size];
+    Capybara_records = new Capybara[cnt_seat];
 }
 
 bool Wagon :: addCapybara(Capybara newCapy){
+    delete [] Capybara_records;
+    Capybara_records = new Capybara[cnt_seat];       
     if (cnt_seat > wagon_size){
         return false;
     }else{
         cnt_seat += 1;
+     
         *(Capybara_records + cnt_seat -1) = newCapy;
         return true;
         
@@ -19,12 +22,17 @@ bool Wagon :: addCapybara(Capybara newCapy){
 void Wagon :: emptyWagon(){
     cnt_seat = 0;
     delete [] Capybara_records;
-    Capybara_records = new Capybara[wagon_size];
+    Capybara_records = new Capybara[cnt_seat];
 }
 
 void Wagon ::printCapybaras(){
     for(int i = 0; i < cnt_seat; i++){
-        std:: cout << (Capybara_records + i) ->getName() << " " <<(Capybara_records + i) -> getAge() << std :: endl;
+        if (cnt_seat > wagon_size){
+            std :: cout << "oops cannot add capybara:" << (Capybara_records + i) ->getName() << " of age " <<(Capybara_records + i) -> getAge() << std :: endl;
+        }else{
+            std:: cout << (Capybara_records + i) ->getName() << " " <<(Capybara_records + i) -> getAge() << std :: endl;
+        }
+        
     }
 
 
