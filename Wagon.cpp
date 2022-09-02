56274f00c5790b0d33bce53d03ba537461dd61bf
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-09-02 19:35:17 +0930

    tired

--- Wagon.cpp
+++ Wagon.cpp
@@ -23,10 +23,14 @@ void Wagon :: emptyWagon(){
 }
 
 void Wagon ::printCapybaras(){
-    if (cnt_seat > 0){
-        for(int i = 0; i < cnt_seat; i++){
+    for(int i = 0; i < cnt_seat; i++){
+        if (i+1 > wagon_size){
+            std :: cout << "oops cannot add capybara:" <<  (Capybara_records + i) ->getName()<<" of age " << (Capybara_records + i) -> getAge() << std :: endl;
+        }else{
             std:: cout << (Capybara_records + i) ->getName() << " " <<(Capybara_records + i) -> getAge() << std :: endl;
+        }
+        
     }
-    }
+
 
 }
